cf3e8edab64487fa3fd0eded2e82675ea94c1d2e
le-garff-yoann <pe.weeble@yahoo.fr>
2017-01-02 17:52:24 +0100

    update copyright interval: 2015-2016 -> 2015-2017

--- lib/Navel/Base/Daemon/Mojolicious/Application/Controller/OpenAPI/Configuration.pm
+++ lib/Navel/Base/Daemon/Mojolicious/Application/Controller/OpenAPI/Configuration.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+# Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 # navel-base-daemon is licensed under the Apache License, Version 2.0
 
 #-> BEGIN
@@ -62,7 +62,7 @@ Navel::Base::Daemon::Mojolicious::Application::Controller::OpenAPI::Configuratio
 
 =head1 COPYRIGHT
 
-Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 
 =head1 LICENSE
 
